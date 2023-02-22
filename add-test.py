b3cb2dd2a2d8b7751f8ccf42044a9895932fef74
Rajat Kashyap <rajatkashyap@Rajats-MBP.T-mobile.com>
2023-02-22 14:34:11 -0600

    Initial

---
+++ add-test.py
@@ -0,0 +1,7 @@
+from add import Add
+
+def test_add():
+    assert Add(2,3)==5
+
+if __name__ == "__main__":
+    test_add()
---
+++ add.py
@@ -0,0 +1,9 @@
+def Add(a,b):
+    return a+b
+
+def say_hello():
+    print "Hello there!"
+
+if __name__ == '__main__':
+    say_hello()
+
